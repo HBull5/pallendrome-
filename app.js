129d1c9fa70f8334a4cbfd366206ca46664a3cc6
hbull5 <harcourtbullv@gmail.com>
2020-05-14 16:41:10 -0400

    start to add styles

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var output = document.querySelector('#output');
-var test1 = 'abc!!cba';
-var test2 = 'apple';
-var test3 = 'racecar';
-var test4 = 'tacocat';
+let output = document.querySelector('#output');
 
-function test(test) {
+function test() {
+    let intput = document.querySelector('#input');
     let j = 0;
-    for (var i = 0; i < test.length; i++) {
-        if (test[i] === test[(test.length - 1) - i]) {
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] === input[(input.length - 1) - i]) {
             j++;
         }
     }
-    if (j === test.length) {
-        output.innerHTML = '"' + test + '"' + ' is a pallendrome';
+    if (j === input.length) {
+        output.innerHTML = '"' + input + '"' + ' is a pallendrome';
     } else {
-        output.innerHTML = '"' + test + '"' + ' is not a pallendrome';
+        output.innerHTML = '"' + input + '"' + ' is not a pallendrome';
     }
 }
-
-test(test2);
